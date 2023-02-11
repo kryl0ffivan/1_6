6d412f25629ae4cf4b6a5a9cc1d32fdb315193da
kryl0ff <kryl0ffivan@gmail.com>
2023-02-11 18:30:47 +0300

    initial commit

--- src/js/menu-burger.js
+++ src/js/menu-burger.js
@@ -1,7 +1,37 @@
-let burgerCloser = document.querySelector('.burger-close');
-let burgerOpen = document.querySelector('.burger');
-let burgerMenu = document.querySelector('.menu-burger');
 
-burgerOpen.addEventListener('click', function () {
-  burgerMenu.classList.toggle('menu-burger--closer')
+const btnMenu = document.querySelector('.burger')
+const menu = document.querySelector('.menu-burger')
+const btnCloser = document.querySelector('.burger-close')
+const mainSect = document.querySelector('.main-section')
+const toggleMenu = function () {
+  menu.classList.add('menu-burger--open')
+  menu.classList.remove('menu-burger--closer')
+  mainSect.classList.add('opacity')
+}
+const closer = function (){
+    menu.classList.remove('menu-burger--open')
+    menu.classList.add('menu-burger--closer')
+    mainSect.classList.remove('opacity')
+    
+}
+
+btnMenu.addEventListener('click', function (e) {
+  e.stopPropagation()
+  toggleMenu()
+})
+
+document.addEventListener('click', function (e) {
+  const target = e.target
+  const its_menu = target == menu || menu.contains(target)
+  const its_btnMenu = target == btnMenu
+  const menu_is_active = menu.classList.contains('menu-burger--open')
+  
+
+ if  (!its_menu && !its_btnMenu && menu_is_active) {
+    closer()
+  }
+})
+
+btnCloser.addEventListener('click', function(){
+  closer()
 })
